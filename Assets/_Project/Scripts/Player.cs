bc261cfc7df5218b4450f752d0c68189181d628a
Merlijn <merlijnvan11@gmail.com>
2024-05-27 15:25:52 +0200

    working on animations

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -1,38 +1,84 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Android;
 
-public class Player : MonoBehaviour {
-    private CustomInput input = null;
-    private Rigidbody2D rb = null;
-    private Vector2 movementVector = Vector2.zero;
-    [SerializeField] private float movementSpeed = 4f;
+namespace timeloop {
+    public class Player : MonoBehaviour {
+        private CustomInput input = null;
+        private Rigidbody2D rb = null;
+        private Vector2 movementVector = Vector2.zero;
+        [SerializeField] private float movementSpeed = 4f;
 
-    private void Awake() {
-        input = new CustomInput();
-        rb = GetComponent<Rigidbody2D>();
-    }
+        // player components
+        private PlayerAnimationController playerAnimationController = null;
 
-    private void OnEnable() {
-        input.Enable();
-        input.Player.Movement.performed += OnMovementPerformed;
-        input.Player.Movement.canceled += OnMovementCancelled;
-    }
+        private void Awake() {
+            input = new CustomInput();
+            rb = GetComponent<Rigidbody2D>();
 
-    private void OnDisable() {
-        input.Disable();
-        input.Player.Movement.performed -= OnMovementPerformed;
-        input.Player.Movement.canceled -= OnMovementCancelled;
-    }
+            // player components
+            playerAnimationController = new PlayerAnimationController(this);
+        }
 
-    private void FixedUpdate() {
-        rb.velocity = movementVector * movementSpeed;
-    }
+        private void OnEnable() {
+            input.Enable();
+            input.Player.Movement.performed += OnMovementPerformed;
+            input.Player.Movement.canceled += OnMovementCancelled;
+        }
 
-    private void OnMovementPerformed(InputAction.CallbackContext value) {
-        movementVector = value.ReadValue<Vector2>();
-    }
+        private void OnDisable() {
+            input.Disable();
+            input.Player.Movement.performed -= OnMovementPerformed;
+            input.Player.Movement.canceled -= OnMovementCancelled;
+        }
 
-    private void OnMovementCancelled(InputAction.CallbackContext value) {
-        movementVector = Vector2.zero;
+        private void FixedUpdate() {
+            rb.velocity = movementVector * movementSpeed;
+        }
+
+        private void Update() {
+            UpdateAnimator(movementVector);
+        }
+
+        private int previousDir = 0;
+
+        private void UpdateAnimator(Vector2 velocity) {
+            int dir = 0;
+            if (velocity.x > 0) {
+                dir = 1;
+            }
+            else if (velocity.x < 0) {
+                dir = 3;
+            }
+            else {
+                if (velocity.y > 0) {
+                    dir = 2;
+                }
+                else if (velocity.y < 0) {
+                    dir = 0;
+                }
+                else {
+                    dir = previousDir;
+                }
+            }
+
+
+            playerAnimationController.SetDirection(dir);
+
+            playerAnimationController.UpdateMove(buttonsPressed);
+            previousDir = dir;
+        }
+
+        private bool buttonsPressed = false;
+
+        private void OnMovementPerformed(InputAction.CallbackContext value) {
+            movementVector = value.ReadValue<Vector2>();
+            buttonsPressed = true;
+        }
+
+        private void OnMovementCancelled(InputAction.CallbackContext value) {
+            movementVector = Vector2.zero;
+            buttonsPressed = false;
+        }
     }
 }
---
+++ Assets/_Project/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace timeloop {
+    public class PlayerAnimationController {
+        private Animator animator;
+        private static readonly int Dir = Animator.StringToHash("Dir");
+        private static readonly int Move = Animator.StringToHash("Move");
+        private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+
+        private bool isMoving;
+
+        public PlayerAnimationController(Player player) {
+            animator = player.GetComponent<Animator>();
+
+#if UNITY_EDITOR
+            if (animator == null) {
+                Debug.LogError("PlayerAnimationController: Animator component not found on Player GameObject.");
+                Object.Destroy(player.gameObject);
+
+                UnityEditor.EditorApplication.isPlaying = false;
+            }
+#endif
+        }
+
+        public void SetDirection(int dir) {
+            if (animator.GetInteger(Dir) == dir) {
+                return;
+            }
+
+            animator.SetInteger(Dir, dir);
+        }
+
+        public void UpdateMove(bool isPlayerMoving) {
+            if (isPlayerMoving && !isMoving) {
+                isMoving = true;
+                animator.SetTrigger(Move);
+                animator.SetBool(IsMoving, true);
+            }
+            else if (!isPlayerMoving) {
+                isMoving = false;
+                animator.SetBool(IsMoving, false);
+            }
+        }
+    }
+}
