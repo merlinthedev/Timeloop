89a303167f3b1234e2203c0d710a5c25da1626c5
Merlijn <merlijnvan11@gmail.com>
2024-05-29 15:36:24 +0200

    camera

--- Assets/_Project/Scripts/src/Player.cs
+++ Assets/_Project/Scripts/src/Player.cs
@@ -1,10 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace timeloop
-{
-    public class Player : MonoBehaviour
-    {
+namespace timeloop {
+    public class Player : MonoBehaviour {
         private CustomInput input = null;
         private Rigidbody2D rb = null;
         private Animator animator;
@@ -13,42 +11,45 @@ namespace timeloop
 
         // player components
 
-        private void Awake()
-        {
+        private void Awake() {
             input = new CustomInput();
             rb = GetComponent<Rigidbody2D>();
             animator = GetComponent<Animator>();
-
         }
 
-        private void OnEnable()
-        {
+        private void OnEnable() {
             input.Enable();
             input.Player.Movement.performed += OnMovementPerformed;
             input.Player.Movement.canceled += OnMovementCancelled;
         }
 
-        private void OnDisable()
-        {
+        private void OnDisable() {
             input.Disable();
             input.Player.Movement.performed -= OnMovementPerformed;
             input.Player.Movement.canceled -= OnMovementCancelled;
         }
 
-        private void FixedUpdate()
-        {
+        private void FixedUpdate() {
             rb.velocity = movementVector * movementSpeed;
-            animator.SetBool("IsMoving", rb.velocity != Vector2.zero);
         }
-        
 
-        private void OnMovementPerformed(InputAction.CallbackContext value)
-        {
+
+        private void Update() {
+            animator.SetBool("IsMoving", rb.velocity != Vector2.zero);
+
+            if (rb.velocity.x > 0) {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (rb.velocity.x < 0) {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+
+        private void OnMovementPerformed(InputAction.CallbackContext value) {
             movementVector = value.ReadValue<Vector2>();
         }
 
-        private void OnMovementCancelled(InputAction.CallbackContext value)
-        {
+        private void OnMovementCancelled(InputAction.CallbackContext value) {
             movementVector = Vector2.zero;
         }
     }
