8d42efbdc386fd785ebeb32e51e666a81a59e7e4
Merlijn <merlijnvan11@gmail.com>
2024-06-04 14:23:05 +0200

    start ability

---
+++ Assets/_Project/Scripts/src/Ability.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace timeloop {
+    public abstract class Ability : MonoBehaviour {
+        [SerializeField] protected float abilityCooldown;
+        protected float abilityTimer = 0f;
+        protected bool canUse => abilityTimer <= 0f;
+
+        public abstract void OnUse();
+
+        protected virtual void Update() {
+            TickAbilityCooldown();
+        }
+
+        private void TickAbilityCooldown() {
+            if (abilityTimer > 0f) {
+                abilityTimer -= Time.deltaTime;
+            }
+        }
+    }
+}
