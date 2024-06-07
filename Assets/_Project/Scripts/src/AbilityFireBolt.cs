0629d44d9a5d2054a2e0c3ba08a77b0cfd5208e6
Merlijn <merlijnvan11@gmail.com>
2024-06-07 16:44:49 +0200

    particle an dability

---
+++ Assets/_Project/Scripts/src/AbilityFireBolt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace timeloop {
+    public class AbilityFireBolt : Ability {
+        [SerializeField] private GameObject fireBoltPrefab;
+        [SerializeField] private float fireBoltSpeed = 20f;
+        
+        public override void OnUse() {
+            
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/src/EntityFireBolt.cs
@@ -0,0 +1,34 @@
+﻿using solobranch.qLib;
+using UnityEngine;
+
+namespace timeloop {
+    public class EntityFireBolt : Entity {
+        private float entitySpeed;
+        private Vector2 normalizedDirection = Vector2.zero;
+        private float damage = 10f;
+
+        public void Constructor(float entitySpeed, float damage, Vector2 normalizedDirection) {
+            this.entitySpeed = entitySpeed;
+            this.damage = damage;
+            this.normalizedDirection = normalizedDirection;
+        }
+
+
+        private void Update() {
+            transform.position += (Vector3)normalizedDirection * entitySpeed * Time.deltaTime;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.gameObject.CompareTag("Player")) {
+                return;
+            }
+
+            if (GameObjectUtilities.HasComponentInHierarchy<EntityLiving>(other.gameObject)) {
+                EntityLiving entityLiving = other.gameObject.GetComponent<EntityLiving>();
+                entityLiving.TakeDamage(this, damage);
+                
+                
+            }
+        }
+    }
+}
