ff78efad43726036892797fb6c6797c9cdc93963
Merlijn <merlijnvan11@gmail.com>
2024-06-12 09:47:47 +0200

    canvas manager

---
+++ Assets/_Project/Scripts/src/CanvasSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace timeloop {
+    [DefaultExecutionOrder(-1)]
+    public class CanvasSingleton : MonoBehaviour {
+        private static Canvas _instance;
+
+        public static Canvas instance {
+            get {
+                if (_instance == null) {
+                    _instance = FindObjectOfType<Canvas>();
+                }
+
+                return _instance;
+            }
+
+            private set { _instance = value; }
+        }
+
+        private void Awake() {
+            DontDestroyOnLoad(gameObject);
+            instance = this.GetComponent<Canvas>();
+        }
+
+        private void OnDestroy() {
+            _instance = null;
+        }
+    }
+}
