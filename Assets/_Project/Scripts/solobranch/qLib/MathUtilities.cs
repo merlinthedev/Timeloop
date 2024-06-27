262c426519c7573aee7eb197983f3bc188eec15a
Merlijn <merlijnvan11@gmail.com>
2024-06-27 15:48:37 +0200

    test

--- Assets/_Project/Scripts/solobranch/qLib/MathUtilities.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace solobranch.qLib {
-    public static class MathUtilities {
-        public static Vector2 GetDirectionFromMouseToOrigin(Vector2 origin) {
-            return ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - origin).normalized;
-        }
-    }
-}
