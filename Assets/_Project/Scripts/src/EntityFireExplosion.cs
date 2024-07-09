3e2b256110ce7bf494ad2dce4b255760b76c39a7
Merlijn <merlijnvan11@gmail.com>
2024-07-10 01:02:59 +0200

    fjkjfajksdfl;fajdfkaljsf;d

---
+++ Assets/_Project/Scripts/src/EntityFireExplosion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using solobranch.qLib;
+using UnityEngine;
+
+namespace timeloop {
+    public class EntityFireExplosion : Entity {
+        private Collider2D collider2d;
+        private float damage;
+
+        private void Start() {
+            collider2d.isTrigger = true;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            throw new NotImplementedException();
+        }
+    }
+}
