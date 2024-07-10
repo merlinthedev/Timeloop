6d5aa6a3d139ba1070a12668ccc9c2511ea663e1
merlin <merlijnvan11@gmail.com>
2024-07-10 18:16:25 +0200

    WIP blink and fire ring

--- Assets/_Project/Scripts/src/AbilityFireRing.cs
+++ Assets/_Project/Scripts/src/AbilityFireRing.cs
@@ -1,7 +1,14 @@
-﻿namespace timeloop {
+﻿using UnityEngine;
+
+namespace timeloop {
     public class AbilityFireRing : Ability {
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private int ringCount;
+        [SerializeField] private float ringInterval;
+        
         public override void OnUse(EntityDamager damager) {
             if (!canUse) return;
+            
             
             
             PostAbilityUse();
