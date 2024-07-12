d19f7a1ba2bc8f6ee4a115ca482be8261a38a081
merlin <merlijnvan11@gmail.com>
2024-07-12 22:04:47 +0200

    jasdkfjdskfl;sdafas

--- Assets/_Project/Scripts/src/EntityFireExplosion.cs
+++ Assets/_Project/Scripts/src/EntityFireExplosion.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 namespace timeloop {
     public class EntityFireExplosion : Entity {
+        [SerializeField] private float lifetime = 1f;
+        private bool shouldTick = false;
         private Collider2D collider2d;
         private float damage;
 
         private EntityDamager owner;
 
         private void Start() {
+            collider2d = GetComponent<Collider2D>();
             collider2d.isTrigger = true;
         }
 
         public void Constructor(EntityDamager owner, float damage) {
             this.owner = owner;
             this.damage = damage;
+
+            shouldTick = true;  
+        }
+
+        private void Update() {
+            if (!shouldTick) return;
+            lifetime -= Time.deltaTime;
+            if (lifetime <= 0) {
+                Destroy(gameObject);
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D other) {
